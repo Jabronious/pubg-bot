03ca8d3bfa2b5df495c736a41fe1d8d742908691
Jarrod <jarrodmerryman@gmail.com>
2018-03-13 23:28:21 +0000

    adds the ability to mention a user

--- pubg_bot.rb
+++ pubg_bot.rb
@@ -8,7 +8,21 @@ data_hash = JSON.parse(file)
 bot = Discordrb::Bot.new token: data_hash['CLIENT_SECRET'], client_id: data_hash['CLIENT_ID']
 
 bot.message(with_text: '!howlong-help') do |event|
-  event.respond 'There is nothing that I can do yet, sorry boot that!'
+    event.respond 'Type "howlong" and then mention a specific user in the channel'
+    event.respond 'Example:'
+    event.respond '"howlong"'
+    event.respond '@Jabronious'
+end
+
+bot.message(with_text: '!howlong') do |event|
+    event.respond 'Mention the user you need info about:'
+    event.user.await(:user) do |user_mention|
+        user_id = user_mention.content.tr('<@>', '').to_i
+        mentioned_user = bot.users[user_id]
+        event.respond "#{mentioned_user.username} Current Match:"
+        event.respond "No Match Data"
+        byebug
+    end
 end
 
 bot.run
