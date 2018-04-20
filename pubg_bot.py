4e77310bf7e5b7f1f9b244393e7806a437ba8701
Jarrod <jarrodmerryman@gmail.com>
2018-04-20 15:04:52 +0000

    organizes utils and adds last command

--- pubg_bot.py
+++ pubg_bot.py
@@ -5,7 +5,7 @@ import pdb
 import requests
 from discord.ext import commands
 from pubg_python import PUBG, Shard, exceptions
-import pubg_utils
+import bot_utils
 import sys
 import os
 from datetime import datetime, timedelta
@@ -22,7 +22,6 @@ bot = commands.Bot(command_prefix='!', description=description)
 
 DATA = json.load(open('bot_info.json'))
 PUBG_CLIENT = PUBG(DATA["PUBG_API_KEY"], Shard.PC_NA)
-EMOJIS = ["💩", "👌", "💯", "😁", "😍"]
 
 @bot.event
 @asyncio.coroutine
@@ -32,6 +31,13 @@ def on_ready():
     print(bot.user.id)
     print('------')
 
+##################################
+#                                #
+#                                #
+#        Matches commands        #
+#                                #
+#                                #
+##################################
 @bot.group(pass_context=True)
 @asyncio.coroutine
 def matches(ctx):
@@ -53,9 +59,12 @@ def matches(ctx):
         logging.info(">>>>>>>>>>>>>printing five matches<<<<<<<<<<<<<<<<<<<")
         embed = discord.Embed(title="React to the match you wish to see data for:",
                                 colour=discord.Colour(14066432))
-        for idx, match in enumerate(player.matches[:5]):
-            match_dict[EMOJIS[idx]] = match.id
-            embed.add_field(name=EMOJIS[idx] + ': ', value=match.id)
+
+        #displays a list of matches
+        emoji_list = bot_utils.get_random_emoji_list()
+        for idx, match in enumerate(player.matches[:len(emoji_list)]):
+            match_dict[emoji_list[idx]] = match.id
+            embed.add_field(name=emoji_list[idx] + ': ', value=match.id, inline=False)
         yield from bot.say(embed=embed)
 
         logging.info(">>>>>>>>>>>>>waiting for reaction from %s<<<<<<<<<<<<<<<<<<<", ctx.message.author)
@@ -64,16 +73,52 @@ def matches(ctx):
 
         match = PUBG_CLIENT.matches().get(match_dict[res.reaction.emoji]) #splits message and pulls id
         
-        yield from bot.say("Let me get that match's data.")
-        embed = pubg_utils.build_embed_message(match, player, PUBG_CLIENT, False)
+        yield from bot.say("Let me get " + match_dict[res.reaction.emoji] + " match's data.")
+        embed = bot_utils.build_embed_message(match, player, PUBG_CLIENT, False)
         yield from bot.say(embed=embed)
-        
-@matches.command(name='latest')
+
+@matches.command(name='last', pass_context=True)
 @asyncio.coroutine
-def _latest(ign : str, name='latest-match'):
+def _last(ctx, ign : str, number_of_matches : int):
+    """'EX: <!matches last Jabronious 10> - This will provide 10 matches for Jabronious
+    """
+    try:
+        logging.info(">>>>>>>>>>>>>searching for player %s<<<<<<<<<<<<<<<<<<<", ign)
+        player = PUBG_CLIENT.players().filter(player_names=[ign])
+        player = player[0]
+        logging.info(">>>>>>>>>>>>>player found<<<<<<<<<<<<<<<<<<<")
+    except exceptions.NotFoundError:
+        yield from bot.say('That player does not exist. Make sure the name is identical')
+        return
+
+    match_dict = {}
+    logging.info(">>>>>>>>>>>>>printing " + str(number_of_matches) + " matches<<<<<<<<<<<<<<<<<<<")
+    embed = discord.Embed(title="React to the match you wish to see data for:",
+                            colour=discord.Colour(14066432))
+
+    #displays a list of matches
+    emoji_list = bot_utils.get_random_emoji_list(number_of_matches)
+    for idx, match in enumerate(player.matches[:len(emoji_list)]):
+        match_dict[emoji_list[idx]] = match.id
+        embed.add_field(name=emoji_list[idx] + ': ', value=match.id, inline=False)
+    yield from bot.say(embed=embed)
+
+    logging.info(">>>>>>>>>>>>>waiting for reaction from %s<<<<<<<<<<<<<<<<<<<", ctx.message.author)
+    res = yield from bot.wait_for_reaction(user=ctx.message.author, timeout=20000) #Waits for 10000ms (maybe?) for a user to react.
+    logging.info("reaction occurred from %s", ctx.message.author)
+
+    match = PUBG_CLIENT.matches().get(match_dict[res.reaction.emoji]) #splits message and pulls id
+
+    yield from bot.say("Let me get " + match_dict[res.reaction.emoji] + " match's data.")
+    embed = bot_utils.build_embed_message(match, player, PUBG_CLIENT, False)
+    yield from bot.say(embed=embed)
+
+@matches.command(name='latest', pass_context=True)
+@asyncio.coroutine
+def _latest(ctx, ign : str, name='latest-match'):
     """'latest <in-game name>. Will provide stats from the most recent match.'"""
     try:
-        logging.info(">>>>>>>>>>>>>searching for player %s, subcommand: !latest<<<<<<<<<<<<<<<<<<<")
+        logging.info(">>>>>>>>>>>>>searching for player %s, subcommand: %s<<<<<<<<<<<<<<<<<<<", ign, ctx.subcommand_passed)
         player = PUBG_CLIENT.players().filter(player_names=[ign])
         player = player[0]
         logging.info(">>>>>>>>>>>>>player found<<<<<<<<<<<<<<<<<<<")
@@ -83,21 +128,14 @@ def _latest(ign : str, name='latest-match'):
     match = PUBG_CLIENT.matches().get(player.matches[0].id)
     
     yield from bot.say("Let me get that match's data.")
-    embed = pubg_utils.build_embed_message(match, player, PUBG_CLIENT)
+    embed = bot_utils.build_embed_message(match, player, PUBG_CLIENT)
     yield from bot.say(embed=embed)
 
-@matches.error
+@matches.command(name='date', pass_context=True)
 @asyncio.coroutine
-def ign_error(error, ctx):
-    logging.debug('***********Invoked Command: ' + ctx.invoked_with + ", Invoked Subcommand: " + str(ctx.invoked_subcommand) + "(" + ctx.subcommand_passed + "), "
-                    + "Error: " + str(error) + ", Message author: " + ctx.message.author.name + ", Message: " + ctx.message.content + "***********")
-    yield from bot.say("You need to put a player's in-game name")
-
-@matches.command(name='date')
-@asyncio.coroutine
-def _date(ign : str, *date : int):
+def _date(ctx, ign : str, *date : int):
+    #TODO: ADD LOGGING
     """'date <in-game name> <date> : Format the MM DD YYYY EX: 04 03 2018'"""
-    pdb.set_trace()
     date = datetime(year=date[2], month=date[1], day=date[0])
     matches = PUBG_CLIENT.matches().filter(
         created_at_start=str(date),
@@ -105,7 +143,22 @@ def _date(ign : str, *date : int):
     )
     yield from bot.say("This feature is not available yet.")
 
-@bot.command(pass_context=True)
+@matches.error
+@asyncio.coroutine
+def ign_error(error, ctx):
+    logging.debug('***********Invoked Command: ' + ctx.invoked_with + ", Invoked Subcommand: " + str(ctx.invoked_subcommand) + "(" + ctx.subcommand_passed + "), "
+                    + "Error: " + str(error) + ", Message author: " + ctx.message.author.name + ", Message: " + ctx.message.content + "***********")
+    yield from bot.say("Something Happened, OH NO!! Don't Worry, you just need to make sure you have entered the command correct" + 
+        "or the player's in-game name is identical")
+
+##################################
+#                                #
+#                                #
+#        Admin commands          #
+#                                #
+#                                #
+##################################
+@bot.command(pass_context=True, hidden=True)
 @asyncio.coroutine
 def restart(ctx):
     message = ctx.message
